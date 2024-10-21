870015b13ad36c2ca1d8b2ed656b64c1110c4597
Christian Manzaraz <magnusmanz@gmail.com>
2024-10-21 11:53:01 -0300

    Initial Commit

---
+++ Clases/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Clases
+//
+//  Created by Christian Manzaraz on 21/10/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
